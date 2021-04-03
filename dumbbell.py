8d5579f4e102f258c29a1c861d266a0ae977d8bc
Denny Gosting <dgosting@gmail.com>
2021-04-03 08:12:16 -0500

    trying longer run

--- dumbbell.py
+++ dumbbell.py
@@ -214,7 +214,7 @@ if __name__ == '__main__':
 
     algorithms = ['cubic'] #, 'reno', 'bbr', 'westwood']
     delays = [21] #, 81, 162]
-    runtime = 60
+    runtime = 600
 
     for _algorithm in algorithms:
         for _delay in delays:
