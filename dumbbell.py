45c7688e43f7804fc93ed5843c0ee08eb5635135
Denny Gosting <dgosting@gmail.com>
2021-04-02 21:27:48 -0500

    trying nohub

--- dumbbell.py
+++ dumbbell.py
@@ -42,21 +42,20 @@ class Dumbbell(Topo):
         s3 = self.addSwitch('s3')
         s4 = self.addSwitch('s4')
 
-        # Link the left and right backbone routers with a delay
-        delayStr = str(delay) + 'ms'
-        self.addLink(s1, s2, bw=984, delay=delayStr, use_htb=True)
-
-        # Link the left access router to the left backbone router
-        self.addLink(s1, s3, bw=252, max_queue_size=.2 * 21 * delay, use_htb=True)
-
-        # Link the right access router to the right backbone router
-        self.addLink(s2, s4, bw=252, max_queue_size=.2 * 21 * delay, use_htb=True)
-
         # Add hosts
         h1 = self.addHost('h1')
         h2 = self.addHost('h2')
         h3 = self.addHost('h3')
         h4 = self.addHost('h4')
+
+        # Link the left access router to the left backbone router
+        self.addLink(s3, s1, bw=252, max_queue_size=.2 * 21 * delay, use_htb=True)
+
+        # Link the left and right backbone routers with a delay
+        self.addLink(s1, s2, bw=984, delay=str(delay) + 'ms', loss=1, use_htb=True)
+
+        # Link the right access router to the right backbone router
+        self.addLink(s4, s2, bw=252, max_queue_size=.2 * 21 * delay, use_htb=True)
 
         # Link two hosts to the left access router
         self.addLink(h1, s3, bw=960)
@@ -112,25 +111,29 @@ def run_test(algorithm, delay, run_time):
     topo = Dumbbell(delay)
 
     # Create an instance of mininet and start it up
-    net = Mininet(topo=topo, controller=OVSController, link=TCLink)
+    net = Mininet(topo=topo, controller=OVSController, link=TCLink, autoSetMacs=True)
 
     print("Starting the Mininet instance")
     net.start()
 
     h1, h2, h3, h4 = net.getNodeByName('h1', 'h2', 'h3', 'h4')
 
+    CLI(net)
+
     print("Starting Fairness test at", datetime.now().strftime("%H:%M:%S"))
 
     print("Starting TCP Flow #1")
-    h3.cmd('iperf3 -s -p 5001 -i 1 > {0} &'.format(h3_iperf_file))
-    h1.cmd('iperf3 -c {0} -p 5001 -M 1500 -C {1} -t {2} &'.format(h3.IP(), algorithm, run_time))
+    h3.cmd('nohup iperf3 -s -p 5001 -i 1 > {0} &'.format(h3_iperf_file))
+    h1.cmd('nohup iperf3 -c {0} -p 5001 -M 1500 -C {1} -t {2} > h1_client.txt &'.format(h3.IP(), algorithm, run_time))
 
     print("Starting TCP Flow #2")
-    h4.cmd('iperf3 -s -p 5002 -i 1 > {0} &'.format(h4_iperf_file))
-    h2.cmd('iperf3 -c {0} -p 5002 -M 1500 -C {1} -t {2} &'.format(h4.IP(), algorithm, run_time))
+    h4.cmd('nohup iperf3 -s -p 5002 -i 1 > {0} &'.format(h4_iperf_file))
+    h2.cmd('nohup iperf3 -c {0} -p 5002 -M 1500 -C {1} -t {2} > h2_client.txt &'.format(h4.IP(), algorithm, run_time))
 
     time.sleep(run_time + 2)
 
+    h1.cmd(kill_iperf)
+    h2.cmd(kill_iperf)
     h3.cmd(kill_iperf)
     h4.cmd(kill_iperf)
 
@@ -138,8 +141,8 @@ def run_test(algorithm, delay, run_time):
 
     print("Starting TCP Flow #1")
     flow1_run_time = 2 * run_time
-    h3.cmd('iperf3 -s -p 5003 &')
-    h1.cmd('iperf3 -c {0} -p 5003 -i 1 -M 1500 -C {1} -t {2} > {3} &'.format(h3.IP(), algorithm, flow1_run_time, h1_iperf_file))
+    h3.cmd('nohup iperf3 -s -p 5003 &')
+    h1.cmd('nohup iperf3 -c {0} -p 5003 -i 1 -M 1500 -C {1} -t {2} > {3} &'.format(h3.IP(), algorithm, flow1_run_time, h1_iperf_file))
 
     offset = int(runtime * .25)
 
@@ -149,11 +152,13 @@ def run_test(algorithm, delay, run_time):
     print("Starting TCP Flow #2")
     flow2_run_time = int(1.75 * run_time)
 
-    h4.cmd('iperf3 -s -p 5004 &')
-    h2.cmd('iperf3 -c {0} -p 5004 -i 1 -M 1500 -C {1} -t {2} > {3} &'.format(h4.IP(), algorithm, flow2_run_time, h2_iperf_file))
+    h4.cmd('nohup iperf3 -s -p 5004 &')
+    h2.cmd('nohup iperf3 -c {0} -p 5004 -i 1 -M 1500 -C {1} -t {2} > {3} &'.format(h4.IP(), algorithm, flow2_run_time, h2_iperf_file))
 
     time.sleep(flow2_run_time + 2)
 
+    h1.cmd(kill_iperf)
+    h2.cmd(kill_iperf)
     h3.cmd(kill_iperf)
     h4.cmd(kill_iperf)
     net.stop()
@@ -161,34 +166,34 @@ def run_test(algorithm, delay, run_time):
     print("Finished test at ", datetime.now().strftime("%H:%M:%S"), ". Now parsing data from files....")
 
     # Make plot for CWND
-    h1_time, h1_throughput, h1_cwnd = parse_iperf_data(h3_iperf_file, 0)
-    h2_time, h2_throughput, h2_cwnd = parse_iperf_data(h4_iperf_file, offset)
-    fig, ax = plt.subplots()
-    ax.plot(h1_time, h1_cwnd, label="TCP Flow 1", linewidth=0.6)
-    ax.plot(h2_time, h2_cwnd, label="TCP Flow 2", linewidth=0.6)
-
-    ax.set(xlabel='Time (seconds)',
-           ylabel='Congestion Window (packets)',
-           title=algorithm.upper() + " with Delay: " + str(delay) + "ms")
-    ax.legend()
-    file_name = "cwnd_" + algorithm + "_" + str(delay) + "ms.png"
-    fig.savefig(file_name)
-
-    # Make plot for Fairness
-    h1_time, h1_throughput, h1_cwnd = parse_iperf_data(h1_iperf_file, 0)
-    h2_time, h2_throughput, h2_cwnd = parse_iperf_data(h2_iperf_file, offset)
-    fig, ax = plt.subplots()
-
-    ax.plot(h1_time, h1_throughput, label="TCP Flow 1", linewidth=0.6)
-    ax.plot(h2_time, h2_throughput, label="TCP Flow 2", linewidth=0.6)
-
-    ax.set(xlabel='Time (seconds)',
-           ylabel='Throughput (Mbps)',
-           title=algorithm.upper() + " with Delay: " + str(delay) + "ms")
-
-    ax.legend()
-    file_name = "fairness_" + algorithm + "_" + str(delay) + "ms.png"
-    fig.savefig(file_name)
+    # h1_time, h1_throughput, h1_cwnd = parse_iperf_data(h3_iperf_file, 0)
+    # h2_time, h2_throughput, h2_cwnd = parse_iperf_data(h4_iperf_file, offset)
+    # fig, ax = plt.subplots()
+    # ax.plot(h1_time, h1_cwnd, label="TCP Flow 1", linewidth=0.6)
+    # ax.plot(h2_time, h2_cwnd, label="TCP Flow 2", linewidth=0.6)
+    #
+    # ax.set(xlabel='Time (seconds)',
+    #        ylabel='Congestion Window (packets)',
+    #        title=algorithm.upper() + " with Delay: " + str(delay) + "ms")
+    # ax.legend()
+    # file_name = "cwnd_" + algorithm + "_" + str(delay) + "ms.png"
+    # fig.savefig(file_name)
+    #
+    # # Make plot for Fairness
+    # h1_time, h1_throughput, h1_cwnd = parse_iperf_data(h1_iperf_file, 0)
+    # h2_time, h2_throughput, h2_cwnd = parse_iperf_data(h2_iperf_file, offset)
+    # fig, ax = plt.subplots()
+    #
+    # ax.plot(h1_time, h1_throughput, label="TCP Flow 1", linewidth=0.6)
+    # ax.plot(h2_time, h2_throughput, label="TCP Flow 2", linewidth=0.6)
+    #
+    # ax.set(xlabel='Time (seconds)',
+    #        ylabel='Throughput (Mbps)',
+    #        title=algorithm.upper() + " with Delay: " + str(delay) + "ms")
+    #
+    # ax.legend()
+    # file_name = "fairness_" + algorithm + "_" + str(delay) + "ms.png"
+    # fig.savefig(file_name)
 
 
 if __name__ == '__main__':
